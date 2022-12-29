af785fccd3af284a45be81da0e8fab64a66808ee
kalyevauli <toktosunovaurkuya003@gmail.com>
2022-12-29 20:51:17 +0300

    collection

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const swiper = new swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
